bdd1b3a7c3f502556a298dbdb1783caee266e580
tdubourg <tdubourg@users.noreply.github.com>
2014-06-24 10:29:30 +0200

    Added a new player type, a cheater that will...well, try to cheat

--- player.py
+++ player.py
@@ -39,3 +39,37 @@ class DummyPlayer(AbstractPlayer):
             self.resources[Resources.CASH]/2
         )
         self.interface['make_transaction'](tr)
+
+class CheaterPlayer(AbstractPlayer):
+    """I am going to attempt to cheat"""
+
+    def __init__(self, *args):
+        super(CheaterPlayer, self).__init__(*args)
+        self.other_player_ids = [_ for _ in self.players_ids if _ is not self.player_id]
+        from random import randrange as rr, choice as rc
+        self.c = rc
+        self.rr = rr
+    
+    def random_scheduled_transaction(self, current_round):
+        # To appeal other players, we offer them X > Y cash in exchange of Y cash (in other words: free cash!)
+        amount1 = self.rr(0, 1000000)
+        amount2 = self.rr(amount1, 1000000)
+        return ScheduledBidirectionalTransaction(
+            self.c(self.other_player_ids),  # Just transfer money to the player that is player just before us
+            self.player_id,
+            Resources.CASH,
+            amount1,
+            None,
+            Resources.CASH,
+            amount2,
+            self.rr(current_round, 10000)
+        )
+
+    def play_round(self, round_number):
+        # Try to make schedule transaction with anyone that accepts it, to sell whatever they accepts
+        # and try to set a impossible deadline (> the end of the game)
+        # or just never pay them back
+        #@TODO
+        tr = self.random_scheduled_transaction(round_number)
+        while not self.interface['make_transaction'](tr):
+            tr = self.random_scheduled_transaction(round_number)
