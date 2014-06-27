40ce1c26f8af552a25ddf5c73196ace02e2fa804
tdubourg <tdubourg@users.noreply.github.com>
2014-06-27 17:17:43 +0200

    Compilation script might be USEFUL in the end

---
+++ paper/compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+f=`find . -name '*.md' | head -n1`
+echo "Found file $f to compile..."
+
+# MMD_TEXT_FILES_DIR is where you downloaded https://github.com/fletcher/peg-multimarkdown-latex-support
+dir=$MMD_TEX_FILES_DIR
+orig=`pwd`
+
+echo "Switching to compilation dir $dir"
+cp $f $dir
+cp *.png $dir -v
+cd $dir
+
+echo "Compiling to TeX..."
+multimarkdown --to=latex $f > "$f.tex"
+
+echo "Compiling to PDF..."
+if pdflatex "$f.tex"; then
+	echo "PDF compiled with success. Cleaning LateX mess..."
+	rm -vf "$f.log"
+else
+	echo "PDF Compilation failed"
+fi
+
+
+echo "Opening PDF..."
+evince "$f.pdf" & 
+
+echo "Switching back to original directory $orig"
+cd $orig
