d4997101ed5de2e9d4382a833a0daec77b6f4e82
tdubourg <tdubourg@users.noreply.github.com>
2014-07-20 20:20:20 +0200

    Updating compilation file + screw you, LateX

--- paper/compile.sh
+++ paper/compile.sh
@@ -2,7 +2,6 @@
 
 f=`find . -name '*.md' | head -n1`
 echo "Found file $f to compile..."
-
 # MMD_TEXT_FILES_DIR is where you downloaded https://github.com/fletcher/peg-multimarkdown-latex-support
 dir=$MMD_TEX_FILES_DIR
 orig=`pwd`
@@ -10,20 +9,34 @@ orig=`pwd`
 echo "Switching to compilation dir $dir"
 cp $f $dir
 cp *.png $dir -v
+# cp *.tex $dir -v we do not have includes for now, let this be unused
 cd $dir
 
 echo "Compiling to TeX..."
 multimarkdown --to=latex $f > "$f.tex"
 
+# Generate a file with line returns, that we will use as stdin input
+# to pdflatex to tell him go fuck itself
+echo "" > /tmp/lines.txt
+echo "" >> /tmp/lines.txt
+echo "" >> /tmp/lines.txt
+echo "" >> /tmp/lines.txt
+echo "" >> /tmp/lines.txt
+echo "" >> /tmp/lines.txt
+echo "" >> /tmp/lines.txt
+
 echo "Compiling to PDF..."
-if pdflatex "$f.tex"; then
+if pdflatex "$f.tex" < /tmp/lines.txt; then
 	echo "PDF compiled with success."
 else
 	echo "PDF Compilation failed"
 fi
 
 echo "Opening PDF..."
-evince "$f.pdf" & 
+cp "${f}.pdf" "${orig}/"
 
 echo "Switching back to original directory $orig"
 cd $orig
+
+evince "$f.pdf" & 
+
